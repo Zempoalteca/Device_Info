881f183e3e73ae3adb0bae539aec512972e5e2f1
Gabriel Zempoalteca <gabriel.zmpltc@gmail.com>
2021-06-23 14:39:14 -0500

    feat: General
    Initial project

---
+++ deviceinfo/deviceinfo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  deviceinfo
+//
+//  Created by Gabriel Z on 23/06/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
